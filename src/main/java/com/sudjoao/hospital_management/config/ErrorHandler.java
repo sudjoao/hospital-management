873e0f20bf097c66b536d6e1fc0d6f610501c550
sudjoao <isudjoao@gmail.com>
2024-10-06 15:20:13 -0300

    chore: create initial invalid arguments handler

--- src/main/java/com/sudjoao/hospital_management/config/ErrorHandler.java
+++ src/main/java/com/sudjoao/hospital_management/config/ErrorHandler.java
@@ -2,6 +2,7 @@ package com.sudjoao.hospital_management.config;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -9,7 +10,13 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class ErrorHandler {
 
     @ExceptionHandler(EntityNotFoundException.class)
-    ResponseEntity notFoundError() {
+    ResponseEntity handleNotFoundError() {
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    ResponseEntity handleInvalidArgumentsError(MethodArgumentNotValidException exception) {
+        var erros = exception.getFieldErrors().stream().map(e -> "%s - %s".formatted(e.getField(), e.getDefaultMessage()));
+        return ResponseEntity.badRequest().body(erros);
     }
 }
